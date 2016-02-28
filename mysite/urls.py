38cee98f1ff375575538b17c8afeca07bb22a776
wesky93 <wesky93@gmail.com>
2016-02-28 10:21:58 -0500

    test

---
+++ mysite/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'mysite.views.home', name='home'),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('blog.urls')),
+]
