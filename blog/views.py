a5b613fd78670c228b4ce6eedeeba88e2db2b304
valsimot <valsimot@gmail.com>
2016-03-02 09:16:53 -0500

    ectenc app

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import post
 
@@ -6,3 +6,7 @@ from .models import post
 def post_list(request):
     posts = post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
+
+def post_detail(request, pk):
+  post = get_object_or_404(posts, pk=pk)
+  return render(request,'blog/post_detail.html', {'posts':post})
