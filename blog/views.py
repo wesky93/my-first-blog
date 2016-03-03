5cde9ea580ac4ace2e18761b7652ba90abe453d3
valsimot <valsimot@gmail.com>
2016-03-03 08:02:28 -0500

    form save study

--- blog/views.py
+++ blog/views.py
@@ -13,5 +13,13 @@ def post_detail(request, pk):
   return render(request,'blog/post_detail.html', {'post':posts})
 
 def post_new(request):
-  form = PostForm()
+  if request.method == "POST":
+    form = PostForm(request.POST)
+    if form.is_valid():
+      post = form.save(commit=False)
+      post.author = request.user
+      post.published_date =timezone.now()
+      post.save
+  else:
+    form =PostForm()
   return render(request, 'blog/post_edit.html', {'form': form})
